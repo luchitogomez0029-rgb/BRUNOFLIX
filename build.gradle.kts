084daf84cf243c853b44e7b2f159b62f9d6f08af
Developer <developer@example.com>
2026-06-21 22:45:34 -0500

    Fix Cloudstream library repository path

--- build.gradle.kts
+++ build.gradle.kts
@@ -70,7 +70,7 @@ subprojects {
         val implementation by configurations
 
         // Stubs for all Cloudstream classes
-        compileOnly("com.lagradost:cloudstream3:pre-release")
+        compileOnly("com.github.recloudstream:cloudstream:pre-release")
 
         // these dependencies can include any of those which are added by the app,
         // but you dont need to include any of them if you dont need them
